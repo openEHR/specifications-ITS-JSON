3e6f847e15c54f4239067f8f6a5d56dd9701568c
Sebastian Iancu <sebastian@code24.nl>
2019-01-06 22:09:51 +0100

    fixing href to be inline with the new openEHR specifications website; renamimg RM/latest to RM/Release-1.0.4

---
+++ components/RM/Release-1.0.4/index.php
@@ -0,0 +1,26 @@
+﻿<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_functions.php');
+
+//Change page name here:
+$PageName = 'openEHR JSON Schemas';
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
+?>
+
+		<div id="Content">
+<!-- - - - - - - - - - - - - - - - - - - - - - - Content starts here - - - - - - - - - - - - - - - - - - - - - - - - - -->
+			
+			<h1><?php echo "$PageName";?></h1>
+			
+			<p>The following files are the openEHR RM component models expressed in JSON Schema format.</p>
+			<ul>
+				<?php
+					foreach (glob ("*/*.json") as $file) {
+						echo "<li><a href=\"{$file}\">{$file}</a></li>\n";
+					}
+				?>
+			</ul>
+
+<!-- - - - - - - - - - - - - - - - - - - - - - - Content ends here - - - - - - - - - - - - - - - - - - - - - - - - - -->
+		</div>	
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_footer.php');?>
